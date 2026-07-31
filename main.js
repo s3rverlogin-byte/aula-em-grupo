c0f7167d3f3abb94bc8cc12a9e540ac9c56ab5b3
guilhermeabreudz7-cmd <guilhermeabreudz7@gmail.com>
2026-07-31 16:25:27 -0400

    save3

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+let save=[]
 async function teste(){
     let teste1=await fetch("./main.json");
     let compiler= await teste1.json();
-    console.log(compiler);
+    save=compiler;
+}
+function input(){
+    let teste=document.getElementById("input").value;
+    alert(save[teste]);
 }
 teste();
