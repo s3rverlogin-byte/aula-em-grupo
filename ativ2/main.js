ecd935636863209efca9c62c252509f930240fd6
guilhermeabreudz7-cmd <guilhermeabreudz7@gmail.com>
2026-08-03 14:42:02 -0400

    teste2

--- ativ2/main.js
+++ ativ2/main.js
@@ -1,10 +1,13 @@
+//salva as informações
 let save=[];
 async function compiler(){
+    //pega o arquivo .json
     let arq= await fetch("./main.js");
+    //link o conteudo do .json no "save"
     let json_arq= await arq.json();
     save=json_arq;
 }
-
+//função de imput simples
 function input(){
-    console.log("teste");
+    alert("teste");
 }
